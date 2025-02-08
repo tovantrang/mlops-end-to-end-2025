d33caa19d442b9b342247d1a23094bde425b92b0
tovantrang <nicolas.tovantrang@gmail.com>
2025-02-08 15:42:31 +0100

    feat (data): add data piepline script

--- pipelines/data_pipeline.py
+++ pipelines/data_pipeline.py
@@ -1,15 +1,12 @@
 import subprocess
 
 if __name__ == "__main__":
-    scripts = ["src/data_load.py", "src/data_validation.py", "src/data_pipeline.py"]
+    scripts = ["src/data_load.py", "src/data_validation.py", "src/data_preparation.py"]
 
     print("Démarrage du pipeline data...")
     for script in scripts:
         print(f"Exécution de {script}...")
-        result = subprocess.run(["python", script], capture_output=True, text=True)
-
-        print(result.stdout)
-        print(result.stderr)
+        result = subprocess.run(["python", script], text=True)
 
         if result.returncode != 0:
             print(f"Erreur lors de l'exécution de {script}")
