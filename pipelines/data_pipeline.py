d19c76d2ab8e456d20e12dc838b04c881215de0f
tovantrang <nicolas.tovantrang@gmail.com>
2025-02-08 15:14:00 +0100

    chore(data): add tqdm for progress visibility

---
+++ pipelines/data_pipeline.py
@@ -0,0 +1,18 @@
+import subprocess
+
+if __name__ == "__main__":
+    scripts = ["src/data_load.py", "src/data_validation.py", "src/data_pipeline.py"]
+
+    print("Démarrage du pipeline data...")
+    for script in scripts:
+        print(f"Exécution de {script}...")
+        result = subprocess.run(["python", script], capture_output=True, text=True)
+
+        print(result.stdout)
+        print(result.stderr)
+
+        if result.returncode != 0:
+            print(f"Erreur lors de l'exécution de {script}")
+            break
+
+    print("Pipeline terminée.")
