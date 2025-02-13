70b3dd31d542d4de6124084e3b9826db2ae8a4fd
tovantrang <nicolas.tovantrang@gmail.com>
2025-02-13 16:01:55 +0100

    fix (train): fix the call for yolo training

--- src/training.py
+++ src/training.py
@@ -1,11 +1,11 @@
-from config import settings
 import mlflow
+from config import settings
 from mlflow import MlflowClient
 from ultralytics import YOLO
 
 
 class Trainer:
-    def __init__(self, model : str) -> None:
+    def __init__(self, model: str) -> None:
         """
         This class is used to train the model and save it
 
@@ -14,29 +14,34 @@ class Trainer:
         """
         self.model = YOLO(model)
 
-    def train(self, conf_file : str, run_name : str,
-              mlflow_local_path : str ="requirements.txt", mlflow_artifact_path : str ="environment"):
+    def train(
+        self,
+        conf_file: str,
+        run_name: str,
+        mlflow_local_path: str = "requirements.txt",
+        mlflow_artifact_path: str = "environment",
+    ):
         """
         This function is used to train the model
 
         Args:
             conf : path of the configuration file for the training data
-            run_name : 
+            run_name :
             epochs : number of epochs
             device : identifier for the device use to train the model
             save_dir : directory in which save the model
             mlflow_local_path :
-            mlflow_artifact_path : 
+            mlflow_artifact_path :
         """
         with mlflow.start_run(run_name=run_name, log_system_metrics=True) as run:
-            self.model.train(data=conf_file)
+            self.model.train(cfg=conf_file)
             mlflow.log_artifact(
                 local_path=mlflow_local_path,
                 artifact_path=mlflow_artifact_path,
                 run_id=run.info.run_id,
             )
 
-    def register(self, model_name : str) -> None:
+    def register(self, model_name: str) -> None:
         """
         This function is used to save the model
 
@@ -52,7 +57,7 @@ class Trainer:
 
         client = MlflowClient()
         alias = "test"
-        '''
+        """
         champion_runs = client.search_runs(tag="Champion")
         if champion_runs == []:
             alias = "Champion"
@@ -61,7 +66,7 @@ class Trainer:
                 alias = "Challenger"
             else:
                 alias = "failure?"
-        '''
+        """
         client.set_registered_model_alias(
             name=model_name,
             alias=alias,
@@ -72,8 +77,10 @@ class Trainer:
 if __name__ == "__main__":
     trainer = Trainer(settings.model)
 
-    trainer.train(conf_file=settings.conf_file, 
-                  run_name=settings.run_name, 
-                  mlflow_local_path=settings.mlflow_local_path,
-                  mlflow_artifact_path=settings.mlflow_artifact_path)
+    trainer.train(
+        conf_file=settings.conf_file,
+        run_name=settings.run_name,
+        mlflow_local_path=settings.mlflow_local_path,
+        mlflow_artifact_path=settings.mlflow_artifact_path,
+    )
     trainer.register(model_name=settings.model_name)
