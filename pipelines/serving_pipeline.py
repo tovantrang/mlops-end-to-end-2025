2957e48447b3de95fbacf63169232a89f5084d88
tovantrang <nicolas.tovantrang@gmail.com>
2025-02-16 11:56:00 +0100

    feat (serving): add serving pipeline and test script

---
+++ pipelines/serving_pipeline.py
@@ -0,0 +1,35 @@
+import subprocess
+
+import bentoml
+
+
+BENTO_NAME = "YOLOservice"
+DEPLOYMENT_NAME = "my-yolo-deployment"
+
+
+def run_command(command: str):
+    """Execute a shell command and return the return code.
+
+    Args:
+        command (str): The command to execute
+    """
+    process = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+    )
+    process.wait()
+    return process.returncode
+
+
+if __name__ == "__main__":
+    run_command("python src/download_champion.py")
+    run_command("bentoml cloud login")
+
+    dep = bentoml.deployment.create(bento=".", name="yolo-deployment")
+    print(
+        f"Le déploiement a été créé avec succès : {dep} et est en cours de déploiement."
+    )
+
+    dep.wait_until_ready(timeout=3600)
+
+    deployment_info = bentoml.deployment.get("yolo-deployment")
+    print("endpoint url:", deployment_info.get_endpoint_urls())
